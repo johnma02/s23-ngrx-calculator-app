11b073c71a5f386b5fad855e99c3b64735451f53
Jonathan Ma <ma.jonathan02@gmail.com>
2023-06-08 08:44:43 -0400

    feat: add calculator actions

---
+++ src/app/calculator.actions.ts
@@ -0,0 +1,7 @@
+import { createAction } from '@ngrx/store';
+
+export const add = createAction('[Calculator Component] Add');
+export const multiply = createAction('[Calculator Component] Multiply');
+export const subtract = createAction('[Calculator Component] Subtract');
+export const divide = createAction('[Calculator Component] Divide');
+export const modulo = createAction('[Calculator Component] Modulo');
